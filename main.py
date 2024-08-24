f4d63d21bbf40c74ec1de9b54bc1b1bd1c1ca373
Anderson <maxxd814@gmail.com>
2024-08-24 00:40:19 -0500

    version 0.0.0.4

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ origins = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
     "http://localhost:3000",
-    "http://localhost:5000"
+    "http://localhost:5000",
+    "https://frontprueba-ybvt.onrender.com",
 ]
 
 app.add_middleware(
